ffd0fc3f7430f4d0685eee5eda7683ed06fd6c2d
Max <mjvcarroll@gmail.com>
2025-11-29 20:01:53 +0000

    Package pyfoam

--- flake.nix
+++ flake.nix
@@ -1,23 +1,34 @@
-{
-  description = "Hazel dev switch";
+{  description = "Kayak Hull Optimisation";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
   };
 
-  outputs = { self , nixpkgs ,... }: let
-    system = "x86_64-linux";
-  in {
-    devShells."${system}".default = let
-      pkgs = import nixpkgs {
-        inherit system;
+  outputs = { self, nixpkgs, ... }: 
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      python = pkgs.python312;
+      pyfoam = python.pkgs.buildPythonPackage rec {
+        pname = "PyFoam"; 
+        version = "2023.7";
+        format = "wheel";
+        src = python.pkgs.fetchPypi {
+          inherit pname version format;
+          sha256 = "sha256-qLCM7hnwqD+OuTl2OBOfMQWX16W89FBatjoGl/YNFFY=";
+        };
       };
-    in pkgs.mkShell {
-      # create an environment with nodejs_18, pnpm, and yarn
-      packages = with pkgs; [
-        python3
-        just
-      ];
+  in {
+    devShells."${system}".default = 
+      pkgs.mkShell {
+        packages = with pkgs; [
+          just
+	  (python.withPackages (p: with p; [
+            numpy
+            pyfoam
+            matplotlib
+          ]))
+        ];
 
       shellHook = ''
       '';
