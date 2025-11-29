eff5cbefde008b6d0fa77f4861b63bd5a05dc76b
Max <mjvcarroll@gmail.com>
2025-11-29 17:34:14 +0000

    flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "Hazel dev switch";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self , nixpkgs ,... }: let
+    system = "x86_64-linux";
+  in {
+    devShells."${system}".default = let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in pkgs.mkShell {
+      # create an environment with nodejs_18, pnpm, and yarn
+      packages = with pkgs; [
+        python3
+        just
+      ];
+
+      shellHook = ''
+      '';
+    };
+  };
+}
